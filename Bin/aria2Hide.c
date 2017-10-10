f0ff6e8b6cc31cbccd6c308d7654451326b80815
RossWang <RossWang@users.noreply.github.com>
2017-10-10 15:56:35 +0800

    Add "stop-with-process" command

--- Bin/aria2Hide.c
+++ Bin/aria2Hide.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
 #include<windows.h>
+#include <tlhelp32.h>
 
 int main (int argc, char **argv){
+	
+	// get parent pid
+	int ppid = -1;
+	int pid = -1;
+    HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32 pe = { 0 };
+    pe.dwSize = sizeof(PROCESSENTRY32);
+
+    pid = GetCurrentProcessId();
+
+    if( Process32First(h, &pe)) {
+        do {
+            if (pe.th32ProcessID == pid) {
+                ppid = pe.th32ParentProcessID;
+            }
+        } while( Process32Next(h, &pe));
+    }
+
+    CloseHandle(h);
+	
+	// start aria2c
 	PROCESS_INFORMATION pi;
 	STARTUPINFO si;
 	ZeroMemory(&si, sizeof(si));
@@ -9,7 +31,13 @@ int main (int argc, char **argv){
 	char cmd[65535] = {0};
     sprintf(cmd, "%s\\..\\", argv[0]);
 	chdir(cmd);
-	CreateProcess(NULL,"aria2c.exe -D --conf-path=aria2.conf",NULL,NULL,0,CREATE_NO_WINDOW,NULL,NULL,&si,&pi);
-
+	if (strcmp(argv[1], "aria2://stop-with-process/") == 0){
+		sprintf(cmd, "aria2c.exe -D --conf-path=aria2.conf --stop-with-process=%d", ppid);
+		CreateProcess(NULL,cmd,NULL,NULL,0,CREATE_NO_WINDOW,NULL,NULL,&si,&pi);
+	}
+ 	else{
+ 		sprintf(cmd, "aria2c.exe -D --conf-path=aria2.conf");
+		CreateProcess(NULL,cmd,NULL,NULL,0,CREATE_NO_WINDOW,NULL,NULL,&si,&pi);
+	}
 	return 0 ;
 	}
