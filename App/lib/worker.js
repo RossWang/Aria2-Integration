7226fc7855ce292789a22c6b40b6139c795035e5
Ross Wang <ross.wwh@gmail.com>
2017-11-19 15:59:22 +0800

    fix an issue of monitor

--- App/lib/worker.js
+++ App/lib/worker.js
@@ -23,7 +23,16 @@ function connect(options) {
 	aria2.onclose = function () {
 		aria2 = null;
 	}
-	aria2.open();
+	aria2.onDownloadStart = function(d) {
+		aria2.tellActive(["gid"]).then((info) => {
+			postMessage(["badge", info.length]);
+			if(info.length == 0) {
+				aria2.close();
+				postMessage("close");
+				close();
+			}
+		});
+	};
 	aria2.onDownloadComplete = function(d) {
 		aria2.getFiles(d.gid).then((info) => {
 			postMessage(["complete", info[0].path]);
@@ -32,6 +41,7 @@ function connect(options) {
 			postMessage(["badge", info.length]);
 			if(info.length == 0) {
 				aria2.close();
+				postMessage("close");
 				close();
 			}
 		});
@@ -41,6 +51,7 @@ function connect(options) {
 			postMessage(["badge", info.length]);
 			if(info.length == 0) {
 				aria2.close();
+				postMessage("close");
 				close();
 			}
 		});
@@ -53,8 +64,19 @@ function connect(options) {
 			postMessage(["badge", info.length]);
 			if(info.length == 0) {
 				aria2.close();
+				postMessage("close");
 				close();
 			}
 		});
 	};
+	aria2.open().then(() => {
+		aria2.tellActive(["gid"]).then((info) => {
+		postMessage(["badge", info.length]);
+		if(info.length == 0) {
+			aria2.close();
+			postMessage("close");
+			close();
+		}
+		});
+	});
 }
